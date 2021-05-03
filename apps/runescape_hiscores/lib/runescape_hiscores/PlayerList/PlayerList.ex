c7a4550a14208cdabbd359c9974fa0ad631e074f
jamessteele <jamessteele@IT-ES-002AM.local>
2021-05-03 15:12:35 -0400

    Implement player list

---
+++ apps/runescape_hiscores/lib/runescape_hiscores/PlayerList/PlayerList.ex
@@ -0,0 +1,27 @@
+defmodule RunescapeHiscores.PlayerList do
+  @moduledoc"""
+  Stores a list of players that can be fetched from the hiscores API
+  """
+
+  use Agent
+
+  def start_link(initial_value) do
+    Agent.start_link(fn -> MapSet.new(initial_value) end, name: __MODULE__)
+  end
+
+  def get do
+    Agent.get(__MODULE__, fn value -> value end)
+  end
+
+  def insert(player) do
+    Agent.update(__MODULE__, fn value ->
+      MapSet.put(value, player)
+    end)
+  end
+
+  def remove(player) do
+    Agent.update(__MODULE__, fn value ->
+      MapSet.delete(value, player)
+    end)
+  end
+end
