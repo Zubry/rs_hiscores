bf9c559ad0496e37388601572e683c902c1f37ba
jamessteele <jamessteele@IT-ES-002AM.local>
2021-05-03 15:50:05 -0400

    Implement player fetcher

---
+++ apps/runescape_hiscores/lib/runescape_hiscores/PlayerFetcher/PlayerFetcher.ex
@@ -0,0 +1,29 @@
+defmodule RunescapeHiscores.PlayerFetcher do
+  require Logger
+
+  # opts is currently ignored, but may be used in the future
+  def fetch(player, _opts \\ []) do
+    player
+    |> generate_url
+    |> HTTPoison.get()
+    |> handle_response(player)
+  end
+
+  defp handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}, player) do
+    Logger.info("Successfully loaded #{player}")
+
+    body
+    |> String.split("\n")
+    |> Enum.map(fn row -> String.split(row, ",") end)
+  end
+
+  defp handle_response(_, player) do
+    Logger.warn("Failed to load #{player}")
+
+    :error
+  end
+
+  defp generate_url(player) do
+    "https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=#{player}"
+  end
+end
