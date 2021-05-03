85771861d61c1fdcca391cdf54c968deab712552
jamessteele <jamessteele@IT-ES-002AM.local>
2021-05-03 15:02:33 -0400

    Add rs hiscores app

---
+++ apps/runescape_hiscores/lib/runescape_hiscores.ex
@@ -0,0 +1,18 @@
+defmodule RunescapeHiscores do
+  @moduledoc """
+  Documentation for `RunescapeHiscores`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RunescapeHiscores.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/runescape_hiscores/test/runescape_hiscores_test.exs
@@ -0,0 +1,8 @@
+defmodule RunescapeHiscoresTest do
+  use ExUnit.Case
+  doctest RunescapeHiscores
+
+  test "greets the world" do
+    assert RunescapeHiscores.hello() == :world
+  end
+end
